a299dee6a4dd8e08a3b5710119fdfbcd9caddb27
Lugard Agu <agulugard@gmail.com>
2023-04-26 18:12:57 +0100

    Add capitalize_decorator, decorator, trimmer_decorator files and update person class

---
+++ person.rb
@@ -0,0 +1,40 @@
+require './nameable'
+require './decorator'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    super()
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
+
+person = Person.new(22, name: 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
