4047bcebc464db995b7967dbb51aad9f61050561
Lugard Agu <agulugard@gmail.com>
2023-04-26 15:28:29 +0100

    Add person, student and teacher classes

---
+++ student.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Student < Person
+  def initialize(age, parent_permission = true, name = "Unknown", classroom = nil)
+    super(age, parent_permission, name)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\\_(ツ)_/¯"
+  end
+end
---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Teacher < Person
+  def initialize(age, parent_permission = true, name = "Unknown", specialization = nil)
+    super(age, parent_permission, name)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
