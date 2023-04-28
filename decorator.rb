a4e4666ea953c13b1c889054257586577a373e42
Lugard Agu <agulugard@gmail.com>
2023-04-28 15:16:33 +0100

    Add association class files

---
+++ decorator.rb
@@ -0,0 +1,14 @@
+require './nameable'
+
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
